28a82f3fc7a5f35e0177a5477b08b84aa5fa407a
Radiant <aks251102@gmail.com>
2026-01-07 00:20:20 +0630

    day2 leetcode RemoveDuplicatesInSortedArray

---
+++ RmDuplicateArr.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 1
+        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+                
+        return k
+
+# Example usage:
+nums = [1, 1, 2, 2, 3]
+solution = Solution()
+k = solution.removeDuplicates(nums)
+print(f"Length of array after removing duplicates: {k}")
