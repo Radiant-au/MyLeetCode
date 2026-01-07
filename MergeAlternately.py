491a2528adc1abaffa5ab06564ed94164899e83b
Radiant <aks251102@gmail.com>
2026-01-08 00:05:59 +0630

    day3 leetcode

--- MergeAlternately.py
+++ MergeAlternately.py
@@ -1,5 +1,5 @@
 class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
+    def MyMergeAlternately(self, word1: str, word2: str) -> str:
         length = min(len(word1), len(word2))
         output=''
         i = 0 
@@ -14,10 +14,21 @@ class Solution:
             output+=word2[i:len(word2)]
             
         return output
+    
+    def BestMergeAlternately(self, word1: str, word2: str) -> str:
+        result = []
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+            i += 1
+        return "".join(result)
 
 
 solution = Solution()
-k = solution.mergeAlternately("abcd","pq")
+k = solution.BestMergeAlternately("abcd","pq")
 print(f"Length of array after merged: {k}")
             
         
