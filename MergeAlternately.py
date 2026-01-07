3cdd2821233b836a545e4350c374284933472c4d
Radiant <aks251102@gmail.com>
2026-01-08 00:11:45 +0630

    remove bullshit

--- MergeAlternately.py
+++ MergeAlternately.py
@@ -16,15 +16,18 @@ class Solution:
         return output
     
     def BestMergeAlternately(self, word1: str, word2: str) -> str:
-        result = []
-        i = 0
-        while i < len(word1) or i < len(word2):
-            if i < len(word1):
-                result.append(word1[i])
-            if i < len(word2):
-                result.append(word2[i])
-            i += 1
-        return "".join(result)
+        res =""
+        i = j =0
+        while i<len(word1) and j<len(word2):
+            res = res + word1[i] +word2[j]
+            i+=1
+            j+=1
+        if i<len(word1):
+            res+=word1[i:]
+        if j<len(word2):
+            res+=word2[j:]
+        return res
+
 
 
 solution = Solution()
