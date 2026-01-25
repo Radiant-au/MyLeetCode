0b851bafc24d56a6998d0744dd3eabe32e5bfccf
Radiant <aks251102@gmail.com>
2026-01-25 23:52:24 +0630

    fix productExceptSelf method to correctly calculate product array

--- ProductArray.py
+++ ProductArray.py
@@ -2,13 +2,19 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        arr = [1] * len(nums)
         prefix = 1 
         for i in range(len(nums)):
-            nums[i] = prefix 
+            arr[i] = prefix 
             prefix *= nums[i]
         
-        return result
-    
+        postfix = 1
+        for i in range(len(nums)-1 , -1 , -1):
+            arr[i] *= postfix
+            postfix *= nums[i]
+            
+        return arr
+        
 solution = Solution()
 arr = [1 ,2 ,3, 4]
 k = solution.productExceptSelf(arr)
