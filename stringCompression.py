eac8a30453cb2fc74190a7eb463b58d63fe01e66
Radiant <aks251102@gmail.com>
2026-01-26 23:55:41 +0630

    solving compress string

---
+++ stringCompression.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        read = 0 
+        write = 0 
+        n = len(chars)
+        while read < n:
+            char = chars[read]
+            count = 0
+             
+            while chars[read]  == char and read < n:
+                 read += 1
+                 count += 1
+                 
+            chars[write] = char
+            write += 1
+            
+            if count > 1:
+                for i in str(count):
+                    chars[write] = i 
+                    write += 1
+                    
+        return write
+    
+solution = Solution()
+chars = ["a","a","b","b","c","c","c"]
+k = solution.compress(chars)
+print(k)
+    
