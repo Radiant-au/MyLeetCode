d37d5894d0584459e90e8299073599fef0afe545
Radiant <aks251102@gmail.com>
2026-01-31 00:59:42 +0630

    done

--- maxNumberPairs.py
+++ maxNumberPairs.py
@@ -1,16 +1,56 @@
 from typing import List
+from collections import Counter
 
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
         count = 0
-        for i in range(1 , len(nums)):
-            for j in range(i , len(nums)):
-                if nums[j] == k - nums[i]:
-                    count += 1
+        right = len(nums) - 1
+        left = 0
+        nums.sort()
+        while left < right:
+            if k == nums[left] + nums[right]:
+                left += 1
+                right -= 1
+                count += 1
+            elif k < nums[left] + nums[right]:
+                right -= 1
+            else:
+                left += 1
+                 
         return count 
     
+    def anotherSolution(self , nums : List[int] , k : int) -> int:
+        count = Counter()
+        ops = 0
+
+        for x in nums:
+            need = k - x
+            if count[need] > 0:
+                ops += 1
+                count[need] -= 1
+            else:
+                count[x] += 1
+
+        return ops
+    
+    def anotherCounterSolution(self , nums : List[int] , k : int) -> int:
+        c = Counter(nums)
+        output = 0
+        seen = set()
+
+        for x in c:
+            if x not in seen and (k - x) in c:
+                if x == (k - x):
+                    output += c[x] // 2
+                else:
+                    output += min(c[x], c[k - x])
+                seen.add(x)
+                seen.add(k - x)
+
+        return output
+    
 solution = Solution()
-nums = [1,2,3,4]
-k = 5
-k = solution.maxOperations(nums , k)
+nums = [3,1,3,4,3]
+k = 6
+k = solution.anotherCounterSolution(nums , k)
 print(k)
